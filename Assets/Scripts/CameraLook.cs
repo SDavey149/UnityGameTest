50c92137970d40cbe8a464e2d6b73c916784c764
Scott Davey <me@sdavey.uk>
2015-04-04 14:25:18 +0100

    Added animations to player and enemy

--- Assets/Scripts/CameraLook.cs
+++ Assets/Scripts/CameraLook.cs
@@ -14,13 +14,17 @@ public class CameraLook : MonoBehaviour {
 	}	
 
 	void FixedUpdate() {
+
+
+	}
+
+	void LateUpdate() {
 		float mouseY = Input.GetAxis ("Mouse Y") * lookSpeed;
 		float newPitch = Mathf.Clamp (pitch + mouseY, -80, 70);
 		//make it smooth
 		pitch = Mathf.SmoothDamp (pitch, newPitch, ref pitchVelocity, 0.1f);
 		transform.rotation = Quaternion.Euler (pitch, 0, 0);
-		transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, 0,0);
-
+		transform.localEulerAngles = new Vector3 (transform.localEulerAngles.x, 0, 0);
 	}
 
 }
